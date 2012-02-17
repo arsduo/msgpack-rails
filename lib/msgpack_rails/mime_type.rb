3422e72c6c49118c656b9b8b61c03388311ccc65
Alex Koppel <alex+git@alexkoppel.com>
2012-02-17 16:51:45 +0100

    Register MIME type alias properly

--- lib/msgpack_rails/mime_type.rb
+++ lib/msgpack_rails/mime_type.rb
@@ -1,5 +1,4 @@
-Mime::Type.register "application/x-mpac", :mpac
-Mime::Type.register "application/x-msgpack", :mpac
+Mime::Type.register "application/x-mpac", :mpac, %w( application/x-msgpack )
 
 # Let's action_dispatch parse the params into the params hash when it receives an mpac request.
 # No idea how to get this in there without adding it to this DEFAULT_PARSERS hash.
