6667b3dfe309f701ad8ecdc245dd20062862eac8
Alex Koppel <alex+git@alexkoppel.com>
2012-02-20 14:39:19 +0100

    Improve the renderer

--- lib/msgpack_rails/3/renderer.rb
+++ lib/msgpack_rails/3/renderer.rb
@@ -1,6 +1,9 @@
 # add render option for msgpack
 ActionController::Renderers.add :msgpack do |data, options|
-  msg = data.to_msgpack(options)
+  # if it's an ActiveModel resource, serialize with the options hash
+  # otherwise, generate message pack with out: ""
+  # not happy that to_msgpack has different signatures for different objects >:O
+  opts = data.respond_to?(:serializable_hash) ? options : (options[:out] || "")
   self.content_type ||= Mime::MPAC
-  msg
+  data.to_msgpack(opts)
 end
