0b9c268266e26b6dba0f13410687d55ad49d2654
Alex Koppel <alex+git@alexkoppel.com>
2012-02-23 13:35:17 +0100

    Use charset user-defined

--- lib/msgpack_rails/3/renderer.rb
+++ lib/msgpack_rails/3/renderer.rb
@@ -4,6 +4,6 @@ ActionController::Renderers.add :msgpack do |data, options|
   # otherwise, generate message pack with out: ""
   # not happy that to_msgpack has different signatures for different objects >:O
   opts = data.respond_to?(:serializable_hash) ? options : (options[:out] || "")
-  self.content_type ||= Mime::MPAC
+  self.content_type ||= "#{Mime::MPAC}; charset=x-user-defined"
   data.to_msgpack(opts)
 end
